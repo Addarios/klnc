08ec011b7347824f29352bc6304e31fc8970a469
Addarios <likuskonr@gmail.com>
2026-03-03 21:14:13 +0100

    Hard reset blog

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# To jest kluczowa zmiana:
-python manage.py makemigrations blog
-python manage.py migrate --fake-initial blog || python manage.py migrate blog
+# To polecenie sprawdzi status i spróbuje naprawić bazę
+python manage.py migrate --fake-initial blog
+python manage.py migrate
