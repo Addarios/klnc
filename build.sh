a142f9320237b4d0a5d356b437abc4ca312d6c13
Addarios <likuskonr@gmail.com>
2026-03-03 12:34:36 +0100

    migrationbs

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "--- INSTALACJA PACZEK ---"
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
 
-# Dodaj tę linię na końcu:
+echo "--- ZBIERANIE STATYK ---"
+python manage.py collectstatic --no-input
+
+echo "--- ROBIENIE MIGRACJI ---"
+# Ta linia wymusi szukanie nowych zmian, jeśli Git coś pominął
+python manage.py makemigrations --noinput 
+# Ta linia stworzy tabele w PostgreSQL
+python manage.py migrate --noinput
+
+echo "--- TWORZENIE ADMINA ---"
 python manage.py setup_admin
+
+echo "--- KONIEC BUILD ---"
